57b74695118c3e6869b4f6a6b4f45e7251804331
karanomorishion <karanomori@users.noreply.github.com>
2020-10-10 22:48:29 +0800

    master: init commit

---
+++ src/main/java/org/sybil/deetbase/DeetBaseApplication.java
@@ -0,0 +1,13 @@
+package org.sybil.deetbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeetBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeetBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sybil/deetbase/DeetBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sybil.deetbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeetBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
